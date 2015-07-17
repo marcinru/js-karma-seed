7b115b1180875870c80454d2fa5aab5ada1af91c
Marcin Rudzki <mar.rudzki@gmail.com>
2015-07-17 16:36:53 +0200

    Adding Batman kata

--- src/kata.js
+++ src/kata.js
@@ -1,3 +1,15 @@
-function Person() {
-  return { greet: "Hello!" };
+/* Batman Quotes
+Batman & Robin have gotten into quite a pickle this time. The Joker has mixed up their iconic quotes and also replaced one of the characters in their names, with a number. They need help getting things back in order.
+
+Implement the getQuote method which takes in an array of quotes, and a string comprised of letters and a single number (e.g. "Rob1n") where the number corresponds to their quote indexed in the passed in array.
+
+Return the quote along with the character who said it:
+getQuote(["I am vengeance. I am the night. I am Batman!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"], "Rob1n") 
+  // =>  "Robin: Holy haberdashery, Batman!
+
+Hint: You are guaranteed that the number in the passed in string is placed somewhere after the first 3 characters of the string. The quotes either belong to Batman, Robin, or Joker. */
+
+function getQuote(quotes, hero) {
+    "use strict";
+    return "Name: quote";
 }
