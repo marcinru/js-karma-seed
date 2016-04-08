98241ddd418b78c619819a081e22c779e5fc3f41
Marcin Rudzki <marcin.rudzki@ericsson.com>
2016-04-08 12:26:55 +0200

    Simplifying the solution

--- src/kata.js
+++ src/kata.js
@@ -16,5 +16,5 @@
  */
 
 function getQuote(quotes, hero) {
-    return {R: 'Robin', J: 'Joker', B: 'Batman'}[hero[0]] + ': ' + quotes[hero.match(/\d/)[0]];
+    return {R:'Robin', J:'Joker', B:'Batman'}[hero[0]] + ': ' + quotes[hero.match(/\d+/)];
 }
